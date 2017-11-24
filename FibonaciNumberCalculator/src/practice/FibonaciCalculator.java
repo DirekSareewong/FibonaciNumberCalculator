8cc9fa45a217bbc42f17f320474455dc87c2fc34
Direk Sareewong <D.Sareewong@dazstory.com>
2017-11-24 16:50:17 +0100

    added main class - runner added calculator class to calculate fibonaci number by position

---
+++ FibonaciNumberCalculator/src/practice/FibonaciCalculator.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class FibonaciCalculator {
+	public static int calculateFinonaciFromPosition(int position){
+		
+		//1 1 2 3 5 8 13 21...
+		if(position == 1|| position == 2)
+			return 1;
+		
+		int lastFibonaci = 1;
+		int beforeLastFibonaci = 1;
+		for(int i=2;i<position;i++){
+			int tmp = beforeLastFibonaci + lastFibonaci;
+			beforeLastFibonaci = lastFibonaci;
+			lastFibonaci = tmp;
+		}
+		return lastFibonaci;
+	}
+
+}
---
+++ FibonaciNumberCalculator/src/practice/FibonaciRunner.java
@@ -0,0 +1,20 @@
+package practice;
+
+import java.util.Scanner;
+
+public class FibonaciRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Enter the position of the fibonaci number: ");
+		Scanner in = new Scanner(System.in);
+		int position = in.nextInt();
+		
+		int fibonaciAtPosition = FibonaciCalculator.calculateFinonaciFromPosition(position);
+		System.out.println("The fibonaci at "+position+" position is :"+fibonaciAtPosition);
+
+	}
+
+}
